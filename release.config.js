dee70088f11f2c9d8b46a7eb21d92b3607df315b
dondrzzy <sibo.donald16@gmail.com>
2023-09-06 16:13:09 +0300

    feat: adds semantic release npm plugin (#6)
    This package is used for publishing to a registry. Currently connfigured to pakcage to github

--- release.config.js
+++ release.config.js
@@ -3,10 +3,12 @@ const config = {
   "plugins": [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
+    "@semantic-release/npm",
     [
       "@semantic-release/git",
       {
-        assets: ["dist/**"]
+        assets: ["package.json"],
+        message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
       }
     ],
     "@semantic-release/github"
